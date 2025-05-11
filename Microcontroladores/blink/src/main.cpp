daa15739bccb105601cba43587a18135038b1329
Stephanomejia17 <stephano.mejia@outlook.es>
2025-05-11 13:19:11 -0500

    avance

--- Microcontroladores/blink/src/main.cpp
+++ Microcontroladores/blink/src/main.cpp
@@ -4,17 +4,17 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(18, OUTPUT);
+  pinMode(12, OUTPUT);
   Serial.begin(9600);
   Serial.println("test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(18, HIGH); // Encender Led
+  digitalWrite(12, HIGH); // Encender Led
   Serial.println("test");
   delay(1000);            // Esperar 1 segundo
-  digitalWrite(18, LOW); // Apagar Led
+  digitalWrite(12, LOW); // Apagar Led
   delay(1000);           // Esperar 1 segundo
 }
 
